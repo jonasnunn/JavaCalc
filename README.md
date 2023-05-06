327faf9afd921d93014c706cd3d2ba80b4beb946
jonasnunn <jonasnunn@gmail.com>
2023-05-06 11:27:33 -0600

    finished up README cleaned up unused code

--- README.md
+++ README.md
@@ -3,17 +3,18 @@ This project is a calculator that correctly applies order of operations to a com
 
 This was developed because I have never used Java before and wanted to become familiar with it's syntax. I wanted to chose a project that would require me to use a variety of common language concepts and a calculator allowed me to accomplish this.
 
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/EWSJ5x69r6Y)
 
 # Development Environment
 I created this project in VS Code using the Maven for Java extension.
 This was all written in Java using the util library.
 
 # Useful Websites
-- [Web Site Name](http://url.link.goes.here)
-- [Web Site Name](http://url.link.goes.here)
+- [chatGPT](https://chat.openai.com/)
+- [W3 Schools](https://www.w3schools.com/java/java_user_input.asp)
 
 # Future Work
+- Adding support for ()
 - Adding common constants such as PI and e.
 - Adding common functions ex. cos, sin, tan.
 - Make the process of converting strings to doubles more efficient.
--- src/App.java
+++ src/App.java
@@ -4,24 +4,29 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner myScan = new Scanner(System.in);
         Reader reader = new Reader();
-        
+
+        System.out.println("Welcome to my calculator program. The calculator supports 4 operators\n1. +\n2. - \n3. *\n4. /\nEnter a blank line to quit");
         System.out.println("=============Calculator=============");
         boolean run = true;
         while (run == true) {
             System.out.print(":> ");
             String str = myScan.nextLine();
             
+            // A blank input will exit the loop and end execution of the program.
             if (str == "") {
                     run = false;
                 }
+            // Validate the input and preform math on the input. 
             else {
-                reader.convertInput(str);
+                reader.convertInput(str); // Convert the input to two arrays of numbers and operators.
                 double[] numbers = reader.getNumbers();
                 char[] operators = reader.getOperators();
                 Calculator calc = new Calculator(numbers, operators);
-                double answer = calc.getAnswer();
-                System.out.println(answer);
+                double answer = calc.getAnswer(); // Do the math.
+                String output = String.format("=  %f", answer);
+                System.out.println(output); // Display the answer in a user friendly way.
             }
+            System.out.println("");
         }
         myScan.close();
     }
